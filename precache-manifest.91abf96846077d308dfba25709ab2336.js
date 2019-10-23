76688ce8d632447b9e062414448e386bf23faede
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2019-10-23 15:09:51 +0300

    Updates

--- precache-manifest.91abf96846077d308dfba25709ab2336.js
+++ precache-manifest.91abf96846077d308dfba25709ab2336.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfdf1cdca0db89e3e51a383e99e29389",
+    "revision": "8b6b7365bd95d586f9957b1ec8b9d520",
     "url": "/goit-react-hw-02-reader/./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-reader/./static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "831e1740fb7c5b1c5ad2",
+    "revision": "cb768d133d41eac68773",
     "url": "/goit-react-hw-02-reader/./static/css/main.41a1d87d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-reader/./static/js/2.161835d5.chunk.js"
   },
   {
-    "revision": "831e1740fb7c5b1c5ad2",
-    "url": "/goit-react-hw-02-reader/./static/js/main.900a6c19.chunk.js"
+    "revision": "cb768d133d41eac68773",
+    "url": "/goit-react-hw-02-reader/./static/js/main.e2a168ee.chunk.js"
   },
   {
     "revision": "d9648edb46569be7eb7a",
